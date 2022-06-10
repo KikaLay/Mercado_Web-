f5df6432e46733cac83afcb79626f0f96317bd96
KikaLay <98002729+KikaLay@users.noreply.github.com>
2022-06-09 22:58:40 -0400

    desafio

---
+++ public/indexx.js
@@ -0,0 +1,9 @@
+const agregarCarrito = document.querySelector("#agregar-carrito")
+const modalCerrar = document.querySelector("#modal-cerrar") // es la equis
+const btnCerrar = document.querySelector("#btn-cerrar") // boton de abajo
+const nombre = document.querySelector("#nombre")//falta
+const btnNavbar = document.querySelector("#btn-navbar")// enlazar con el carrito de compras
+const comprar = []
+
+
+
